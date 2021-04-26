55751e60ee0809724f4764e2f3f8d206af893a7b
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-04-26 13:19:52 +0900

    BC199C

--- BC/199/C/main.js
+++ BC/199/C/main.js
@@ -1,9 +1,10 @@
 const main = (args) => {
   const input = args.trim().split("\n");
   const N = Number(input[0]);
-  let str = input[1];
-  let strArr = str.split("");
+  let S = input[1];
+  let SArr = S.split("");
   const Q = Number(input[2]);
+  let isFlipped = false;
 
   for (let i = 3; i < 3 + Q; i++) {
     const T = Number(input[i].split(" ")[0]);
@@ -11,19 +12,33 @@ const main = (args) => {
     if (T === 1) {
       const A = Number(input[i].split(" ")[1]);
       const B = Number(input[i].split(" ")[2]);
-      const chrA = strArr[A - 1];
-      const chrB = strArr[B - 1];
-      strArr[A - 1] = chrB;
-      strArr[B - 1] = chrA;
+
+      if (!isFlipped) {
+        const chrA = SArr[A - 1];
+        const chrB = SArr[B - 1];
+        SArr[A - 1] = chrB;
+        SArr[B - 1] = chrA;
+      } else {
+        const posA = A <= N ? A - 1 + N : A - 1 - N;
+        const posB = B <= N ? B - 1 + N : B - 1 - N;
+        const chrA = SArr[posA];
+        const chrB = SArr[posB];
+        SArr[posA] = chrB;
+        SArr[posB] = chrA;
+      }
     }
 
     if (T === 2) {
-      const strHead = strArr.slice(0, N);
-      const strTail = strArr.slice(N);
-      strArr = [...strTail, ...strHead];
+      isFlipped = !isFlipped;
     }
   }
-  console.log(strArr.join(""));
+
+  if (isFlipped) {
+    const strHead = SArr.slice(0, N);
+    const strTail = SArr.slice(N);
+    SArr = [...strTail, ...strHead];
+  }
+  console.log(SArr.join(""));
 };
 
 // 提出時は以下を実行
