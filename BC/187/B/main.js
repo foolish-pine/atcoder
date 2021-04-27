dd7ed18f3ce087a2ef50b1a6e12d8b8c23629e32
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-04-27 17:50:14 +0900

    BC187B

---
+++ BC/187/B/main.js
@@ -0,0 +1,26 @@
+const main = (args) => {
+  const input = args.trim().split("\n");
+  const N = Number(input[0]);
+  let result = 0;
+
+  for (let i = 1; i <= N; i++) {
+    const x1 = Number(input[i].split(" ")[0]);
+    const y1 = Number(input[i].split(" ")[1]);
+    for (let j = i; j <= N; j++) {
+      const x2 = Number(input[j].split(" ")[0]);
+      const y2 = Number(input[j].split(" ")[1]);
+
+      const inc = (y2 - y1) / (x2 - x1);
+      if (-1 <= inc && inc <= 1) {
+        result++;
+      }
+    }
+  }
+  console.log(result);
+};
+
+// 提出時は以下を実行
+// main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+// ローカルテスト時は以下を実行
+main(require("fs").readFileSync("test.txt", "utf8"));
