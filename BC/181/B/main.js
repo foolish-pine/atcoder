07fa9cd28d22242ae66d7f9afa505270b542a284
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-12 13:03:44 +0900

    BC181B

---
+++ BC/181/B/main.js
@@ -0,0 +1,23 @@
+const main = (args) => {
+  const input = args.trim().split("\n");
+  const N = Number(input[0]);
+  let sum = 0;
+
+  for (let i = 1; i <= N; i++) {
+    const A = Number(input[i].split(" ")[0]);
+    const B = Number(input[i].split(" ")[1]);
+
+    if ((B - A) % 2 !== 0) {
+      sum += ((A + B - 1) / 2) * (B - A) + B;
+    } else {
+      sum += ((A + B) / 2) * (B - A + 1);
+    }
+  }
+  console.log(sum);
+};
+
+// 提出時は以下を実行
+// main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+// ローカルテスト時は以下を実行
+main(require("fs").readFileSync("test.txt", "utf8"));
