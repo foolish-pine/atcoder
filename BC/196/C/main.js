4511fc99b6453e02b0990ba05f0474033b411b9a
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-19 18:03:29 +0900

    BC196C

--- BC/196/C/main.js
+++ BC/196/C/main.js
@@ -1,37 +1,10 @@
 const main = (args) => {
   const input = args.trim().split("\r\n");
-  const N = input[0];
-  const digit = N.length;
+  const N = Number(input[0]);
   let count = 0;
 
-  if (digit === 1) {
-    console.log(0);
-    return;
-  }
-
-  if (digit === 2) {
-    for (let i = 1; i <= Number(N); i++) {
-      if (String(i)[0] === String(i)[1]) count++;
-    }
-    console.log(count);
-    return;
-  }
-
-  if (digit % 2 !== 0) {
-    for (let i = 1; i <= digit - 1; i++) {
-      count += 9 * Math.pow(10, i - 1);
-    }
-  }
-
-  if (digit % 2 === 0) {
-    for (let i = 1; i <= digit / 2 - 1; i++) {
-      count += 9 * Math.pow(10, i - 1);
-    }
-    const max =
-      Number(N.slice(0, digit / 2)) <= Number(N.slice(2, digit))
-        ? Number(N.slice(0, digit / 2))
-        : Number(N.slice(2, digit));
-    for (let i = Math.pow(10, digit / 2 - 1); i <= max; i++) {
+  for (let i = 1; i <= 1000001; i++) {
+    if (Number(String(i) + String(i)) <= N) {
       count++;
     }
   }
