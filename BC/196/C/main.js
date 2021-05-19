e1cbbd1151f4ae27da8a6867acc563c832184aeb
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-19 13:42:22 +0900

    BC196C途中まで

--- BC/196/C/main.js
+++ BC/196/C/main.js
@@ -1,24 +1,41 @@
 const main = (args) => {
   const input = args.trim().split("\r\n");
-  const num = input[0];
-  const digit = num.length;
+  const N = input[0];
+  const digit = N.length;
   let count = 0;
 
-  if (digit % 2 === 0) {
-    count += (10 ** (digit / 2 - 1)) - 1;
-    for (let i = 10 ** (digit - 1); i <= num; i++) {
-    const digit = String(i).length;
-    if (digit % 2 === 0) {
-      const head = String(i).slice(0, digit / 2);
-      const tail = String(i).slice(digit / 2);
-      if (head === tail) {
-        count++;
-      }
+  if (digit === 1) {
+    console.log(0);
+    return;
+  }
+
+  if (digit === 2) {
+    for (let i = 1; i <= Number(N); i++) {
+      if (String(i)[0] === String(i)[1]) count++;
+    }
+    console.log(count);
+    return;
+  }
+
+  if (digit % 2 !== 0) {
+    for (let i = 1; i <= digit - 1; i++) {
+      count += 9 * Math.pow(10, i - 1);
     }
   }
-  } else {
-    count += (10 ** (digit - 2)) - 1;
+
+  if (digit % 2 === 0) {
+    for (let i = 1; i <= digit / 2 - 1; i++) {
+      count += 9 * Math.pow(10, i - 1);
+    }
+    const max =
+      Number(N.slice(0, digit / 2)) <= Number(N.slice(2, digit))
+        ? Number(N.slice(0, digit / 2))
+        : Number(N.slice(2, digit));
+    for (let i = Math.pow(10, digit / 2 - 1); i <= max; i++) {
+      count++;
+    }
   }
+
   console.log(count);
 };
 
