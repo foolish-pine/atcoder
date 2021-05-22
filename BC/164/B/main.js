28cdf0f957c483af405f605dd601ab4985cb1dfe
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-05-22 13:50:02 +0900

    BC164B

--- BC/164/B/main.js
+++ BC/164/B/main.js
@@ -1,6 +1,24 @@
 const main = (args) => {
   const input = args.trim().split("\n");
-  console.log(input);
+  const A = Number(input[0].split(" ")[0]);
+  const B = Number(input[0].split(" ")[1]);
+  const C = Number(input[0].split(" ")[2]);
+  const D = Number(input[0].split(" ")[3]);
+
+  let takahashi = A;
+  let aoki = C;
+  while (takahashi > 0 && aoki > 0) {
+    aoki -= B;
+    if (aoki <= 0) {
+      console.log("Yes");
+      return;
+    }
+    takahashi -= D;
+    if (takahashi <= 0) {
+      console.log("No");
+      return;
+    }
+  }
 };
 
 // 提出時は以下を実行
