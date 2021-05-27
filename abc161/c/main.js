ab7055fa33413b44ba501b3041c00e1c37872630
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-27 17:05:24 +0900

    abc161c

--- abc161/c/main.js
+++ abc161/c/main.js
@@ -1,6 +1,11 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const N = BigInt(input[0].split(" ")[0]);
+  const K = BigInt(input[0].split(" ")[1]);
 
+  console.log(
+    N % K <= K - (N % K) ? (N % K).toString() : (K - (N % K)).toString()
+  );
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
