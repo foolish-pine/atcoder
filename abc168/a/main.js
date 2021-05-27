8a0aff1d22801fdc67225e5946b7d71996270d08
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-27 13:31:01 +0900

    abc168a

--- abc168/a/main.js
+++ abc168/a/main.js
@@ -1,6 +1,15 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const N = input[0];
 
+  const last = N[N.length - 1];
+  if (["2", "4", "5", "7", "9"].includes(last)) {
+    console.log("hon");
+  } else if (["0", "1", "6", "8"].includes(last)) {
+    console.log("pon");
+  } else if (["3"].includes(last)) {
+    console.log("bon");
+  }
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
