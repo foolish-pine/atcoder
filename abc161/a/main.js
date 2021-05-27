3d909f60caea70435e566ecd24c461cfa8a399fc
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-27 15:57:42 +0900

    abc161a

--- abc161/a/main.js
+++ abc161/a/main.js
@@ -1,6 +1,10 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const X = Number(input[0].split(" ")[0]);
+  const Y = Number(input[0].split(" ")[1]);
+  const Z = Number(input[0].split(" ")[2]);
 
+  console.log(`${Z} ${X} ${Y}`);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
