63e587c70119935fb6cb81f6c3b2876e883af1d6
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-27 15:33:55 +0900

    abc168b

--- abc168/b/main.js
+++ abc168/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const K = Number(input[0]);
+  const S = input[1];
 
+  if (S.length <= K) {
+    console.log(S);
+  } else {
+    const truncated = S.slice(0, K) + "...";
+    console.log(truncated);
+  }
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
