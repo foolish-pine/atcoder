1257075653ce595384b7dc876ad04207ac69e9bd
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-27 15:57:49 +0900

    abc161b

--- abc161/b/main.js
+++ abc161/b/main.js
@@ -1,6 +1,11 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const M = Number(input[0].split(" ")[1]);
+  const A = input[1].split(" ").map((n) => Number(n));
 
+  const sum = A.reduce((a, b) => a + b);
+  const selects = A.filter((n) => n >= sum / (4 * M));
+  console.log(selects.length >= M ? "Yes" : "No");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
