5263aa3c452e7caf36b222a0fcae582ab3f0d002
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-28 10:34:01 +0900

    abc159b

--- abc159/b/main.js
+++ abc159/b/main.js
@@ -1,6 +1,22 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const S = input[0];
 
+  console.log(
+    S === S.split("").reverse().join("") &&
+      S.slice(0, (S.length - 1) / 2) ===
+        S.slice(0, (S.length - 1) / 2)
+          .split("")
+          .reverse()
+          .join("") &&
+      S.slice((S.length + 3) / 2 - 1) ===
+        S.slice((S.length + 3) / 2 - 1)
+          .split("")
+          .reverse()
+          .join("")
+      ? "Yes"
+      : "No"
+  );
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
