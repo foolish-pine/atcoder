e8aa95613b74cb0522e27c7c622b43d27570fbfb
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-05-28 16:10:02 +0900

    abc166a

--- abc166/a/main.js
+++ abc166/a/main.js
@@ -1,6 +1,8 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const S = input[0];
 
+  console.log(S === "ARC" ? "ABC" : "ARC");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
