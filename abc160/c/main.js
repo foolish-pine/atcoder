080f02171777acc9ee3bef0530f61e88e941faee
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-05-29 14:50:51 +0900

    abc160c

--- abc160/c/main.js
+++ abc160/c/main.js
@@ -1,6 +1,21 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const K = Number(input[0].split(" ")[0]);
+  const N = Number(input[0].split(" ")[1]);
+  const arr = input[1].split(" ").map((n) => Number(n));
 
+  let sum = 0;
+  const result = [];
+  for (let i = 0; i < N - 1; i++) {
+    sum += arr[i + 1] - arr[i];
+  }
+  result.push(sum);
+  sum += K - arr[N - 1] + arr[0];
+
+  for (let i = 0; i < N - 1; i++) {
+    result.push(sum - (arr[i + 1] - arr[i]));
+  }
+  console.log(result.reduce((a, b) => (a < b ? a : b)));
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
