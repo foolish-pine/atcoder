546c8756e9d709e1da08a4ec3fa4c08e0fec7b57
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-05-31 23:23:13 +0900

    abc171b

--- abc171/b/main.js
+++ abc171/b/main.js
@@ -1,6 +1,16 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const K = Number(input[0].split(" ")[1]);
+  const arr = input[1]
+    .split(" ")
+    .map((n) => Number(n))
+    .sort((a, b) => a - b);
 
+  let sum = 0;
+  for (let i = 0; i < K; i++) {
+    sum += arr[i];
+  }
+  console.log(sum);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
