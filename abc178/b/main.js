8ce274c70d03d2c0470af8b8ba80bf42a95573eb
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-01 13:15:11 +0900

    abc178b

--- abc178/b/main.js
+++ abc178/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const a = BigInt(input[0].split(" ")[0]);
+  const b = BigInt(input[0].split(" ")[1]);
+  const c = BigInt(input[0].split(" ")[2]);
+  const d = BigInt(input[0].split(" ")[3]);
 
+  let x = a * c <= a * d ? a * d : a * c;
+  let y = b * c <= b * d ? b * d : b * c;
+  let z = x <= y ? y : x;
+  console.log(z.toString());
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
