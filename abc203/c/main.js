bcdceb737fe91a0e50287c39d943770b302a5211
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-02 13:06:18 +0900

    abc203c

--- abc203/c/main.js
+++ abc203/c/main.js
@@ -3,35 +3,31 @@ const main = (args) => {
   const N = Number(input[0].split(" ")[0]);
   const K = Number(input[0].split(" ")[1]);
 
-  const obj = {};
+  const arr = [];
   for (let i = 1; i <= N; i++) {
-    const A = input[i].split(" ")[0];
+    const A = BigInt(input[i].split(" ")[0]);
     const B = BigInt(input[i].split(" ")[1]);
 
-    obj[A] = obj[A] ? obj[A] + B : B;
+    arr.push({ A, B });
   }
 
   let money = BigInt(K);
-  let result = BigInt(0);
-  const keys = Object.keys(obj).sort((a, b) => {
-    if (BigInt(a) - BigInt(b) > BigInt(0)) {
+  arr.sort((a, b) => {
+    if (a.A - b.A > 0) {
       return 1;
-    } else if (BigInt(a) - BigInt(b) === BigInt(0)) {
+    } else if (a.A - b.A === 0) {
       return 0;
-    } else if (BigInt(a) - BigInt(b) < BigInt(0)) {
+    } else if (a.A - b.A < 0) {
       return -1;
     }
   });
-  keys.forEach((key) => {
-    if (result + money >= BigInt(key)) {
-      money -= BigInt(key) - result;
-      result += BigInt(key) - result;
-      money += obj[key] ? obj[key] : BigInt(0);
-    } else {
+  arr.forEach((obj) => {
+    if (obj.A > money) {
       return;
     }
+    money += obj.B;
   });
-  console.log((result + money).toString());
+  console.log(money.toString());
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
