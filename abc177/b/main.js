b686e8bc70a8962db3d168b5bf57c5faa8d7c907
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-04 13:13:03 +0900

    abc177b

--- abc177/b/main.js
+++ abc177/b/main.js
@@ -1,5 +1,18 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
+  const t = input[1];
+
+  let result = t.length;
+  for (let i = 0; i <= s.length - t.length; i++) {
+    let _result = 0;
+    const str = s.slice(i, i + t.length);
+    for (let j = 0; j < t.length; j++) {
+      if (str[j] !== t[j]) _result++;
+    }
+    if (result > _result) result = _result;
+  }
+  console.log(result);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
