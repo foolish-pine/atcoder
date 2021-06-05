77b56954ad66f7c573dff4e9ba13aac69dc3ac1d
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-06-05 10:25:20 +0900

    abc167a

--- abc167/a/main.js
+++ abc167/a/main.js
@@ -1,6 +1,10 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
+  const t = input[1];
 
+  const reg = new RegExp(s + "[a-z]");
+  console.log(reg.test(t) ? "Yes" : "No");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
