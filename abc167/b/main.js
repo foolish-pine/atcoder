8e57a9aa5e7b9f221dc03c315fb0b5a20131ff70
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-06-05 10:41:25 +0900

    abc167b

--- abc167/b/main.js
+++ abc167/b/main.js
@@ -1,6 +1,36 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const a = Number(input[0].split(" ")[0]);
+  const b = Number(input[0].split(" ")[1]);
+  const c = Number(input[0].split(" ")[2]);
+  const k = Number(input[0].split(" ")[3]);
 
+  let rest = k;
+  let result = 0;
+  if (a <= rest) {
+    result += a;
+    rest -= a;
+  } else {
+    console.log(rest);
+    return;
+  }
+
+  if (b <= rest) {
+    rest -= b;
+  } else {
+    console.log(result);
+    return;
+  }
+
+  if (c <= rest) {
+    result -= c;
+  } else {
+    result -= rest;
+    console.log(result);
+    return;
+  }
+
+  console.log(result);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
