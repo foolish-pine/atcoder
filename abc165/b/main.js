7591cd50d27b25928c07ffbcb0ff17cfc9b189fa
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-07 13:07:33 +0900

    abc165b

--- abc165/b/main.js
+++ abc165/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const x = input[0];
 
+  let i = 0;
+  let m = 100n;
+  while (m < x) {
+    m = (m * 101n) / 100n;
+    i++;
+  }
+  console.log(i);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
