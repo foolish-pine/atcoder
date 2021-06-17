331a2c9617e4c20ee0f91953de7adae4431033a5
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-17 13:01:42 +0900

    abc153b

--- abc153/b/main.js
+++ abc153/b/main.js
@@ -1,6 +1,9 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const h = Number(input[0].split(" ")[0]);
+  const arr = input[1].split(" ").map((n) => Number(n));
 
+  console.log(h <= arr.reduce((a, b) => a + b) ? "Yes" : "No");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
