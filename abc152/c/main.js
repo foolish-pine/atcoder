4b78f3eb9aba30928989bad338c4ab0a19244ca7
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-18 13:16:04 +0900

    abc152c

--- abc152/c/main.js
+++ abc152/c/main.js
@@ -1,6 +1,17 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
+  const arr = input[1].split(" ").map((n) => Number(n));
 
+  let min = arr[0];
+  let ans = 1;
+  for (let i = 1; i < n; i++) {
+    if (min >= arr[i]) {
+      ans++;
+      min = arr[i];
+    }
+  }
+  console.log(ans);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
