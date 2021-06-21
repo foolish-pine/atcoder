a3e0b5688e3d90e1b55206a0d5fd4eb81684a8fc
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-21 12:26:44 +0900

    abc206b

--- abc206/b/main.js
+++ abc206/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
 
+  let i = 1;
+  let sum = 0;
+  while (sum <= n) {
+    sum += i;
+    if (sum < n) i++;
+  }
+  console.log(i);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
