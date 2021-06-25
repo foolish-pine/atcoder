db051af62c18da42fd0b1d7fd8713a5af4374c78
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-25 12:51:28 +0900

    abc148c

--- abc148/c/main.js
+++ abc148/c/main.js
@@ -1,6 +1,16 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const a = Number(input[0].split(" ")[0]);
+  const b = Number(input[0].split(" ")[1]);
 
+  const GCD = (x, y) => {
+    if (y === 0) {
+      return x;
+    } else {
+      return GCD(y, x % y);
+    }
+  };
+  console.log((a * b) / GCD(a, b));
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
