45f71434590862be2aa36af1d409f35db05d4f79
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-06-28 12:30:55 +0900

    abc146b

--- abc146/b/main.js
+++ abc146/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
+  const s = input[1];
 
+  console.log(
+    s
+      .split("")
+      .map((c) => String.fromCharCode(65 + (((c.charCodeAt(0) + n) % 65) % 26)))
+      .join("")
+  );
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
