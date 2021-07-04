04db05231059ee0009744bc68f47a920f3e5fabc
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-07-04 12:51:26 +0900

    abc141b

--- abc141/b/main.js
+++ abc141/b/main.js
@@ -1,6 +1,21 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
 
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 !== 0) {
+      if (!["L", "U", "D"].includes(s[i])) {
+        console.log("No");
+        return;
+      }
+    } else {
+      if (!["R", "U", "D"].includes(s[i])) {
+        console.log("No");
+        return;
+      }
+    }
+  }
+  console.log("Yes");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
