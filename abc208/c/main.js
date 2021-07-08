294f873be53b28baa5fad8871202ff7698effe81
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-08 17:26:47 +0900

    abc208

--- abc208/c/main.js
+++ abc208/c/main.js
@@ -1,19 +1,21 @@
 const main = (args) => {
   const input = args.trim().split("\n");
   const n = Number(input[0].split(" ")[0]);
-  const k = Number(input[0].split(" ")[1]);
-  const arr = input[1].split(" ").map((n) => Number(n));
+  const k = input[0].split(" ")[1];
+  const a1 = input[1].split(" ").map((n) => Number(n));
 
-  const min = BigInt(k) / BigInt(n);
-  const rest = BigInt(k) % BigInt(n);
-  const sortedArr = [...arr].sort((a, b) => a - b);
+  const answer = new Array(n).fill(BigInt(k) / BigInt(n));
+
+  const a2 = [];
   for (let i = 0; i < n; i++) {
-    if (BigInt(sortedArr.indexOf(arr[i])) < rest) {
-      console.log((min + 1n).toString());
-    } else {
-      console.log(min.toString());
-    }
+    a2.push(i);
   }
+
+  a2.sort((a, b) => a1[a] - a1[b]);
+  for (let i = 0n; i < BigInt(k) % BigInt(n); i++) {
+    answer[a2[i.toString()]] += 1n;
+  }
+  answer.map((n) => console.log(n.toString()));
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
