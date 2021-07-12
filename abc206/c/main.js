4d13f631f1ce6fe2bb2bc692f90934ff1cb3a975
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-12 13:06:46 +0900

    abc206c

--- abc206/c/main.js
+++ abc206/c/main.js
@@ -1,6 +1,19 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
+  const a = input[1].split(" ").map((n) => Number(n));
 
+  const obj = {};
+  for (let i = 0; i < n; i++) {
+    obj[a[i]] = obj[a[i]] ? obj[a[i]] + 1 : 1;
+  }
+
+  const exceptional = Object.values(obj).reduce((a, b) => {
+    return a + (b * (b - 1)) / 2;
+  }, 0);
+
+  const result = (n * (n - 1)) / 2 - exceptional;
+  console.log(result);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
