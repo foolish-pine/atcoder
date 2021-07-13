c6cdf58c6aa10b506ecb9a4ec4a7a18c7746c7ce
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-13 12:10:37 +0900

    abc140b

--- abc140/b/main.js
+++ abc140/b/main.js
@@ -1,6 +1,16 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
+  const an = input[1].split(" ").map((n) => Number(n));
+  const bn = input[2].split(" ").map((n) => Number(n));
+  const cn = input[3].split(" ").map((n) => Number(n));
 
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    if (an[i - 1] + 1 === an[i]) ans += cn[an[i - 1] - 1];
+    ans += bn[an[i] - 1];
+  }
+  console.log(ans);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
