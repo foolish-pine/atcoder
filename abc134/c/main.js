75a575570e5c8f751e22a7104001d4d85d9da634
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-07-14 14:16:18 +0900

    abc134c

--- abc134/c/main.js
+++ abc134/c/main.js
@@ -1,6 +1,21 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
+  const an = [];
+  for (let i = 1; i <= n; i++) {
+    an.push(Number(input[i]));
+  }
 
+  an.sort((a, b) => b - a);
+  const max = an[0];
+  const secondMax = an[1];
+  for (let i = 1; i <= n; i++) {
+    if (Number(input[i]) === max) {
+      console.log(secondMax);
+    } else {
+      console.log(max);
+    }
+  }
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
