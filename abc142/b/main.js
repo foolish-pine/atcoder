975ade82f08895926b407c75705043d9cbee7dd7
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-15 12:09:18 +0900

    abc142b

--- abc142/b/main.js
+++ abc142/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0].split(" ")[0]);
+  const k = Number(input[0].split(" ")[1]);
+  const hn = input[1].split(" ").map((n) => Number(n));
 
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    if (hn[i] >= k) ans++;
+  }
+  console.log(ans);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
