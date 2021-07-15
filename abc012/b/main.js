b01d878e5763ca20d53711d6ba4ee33a823faa69
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-15 16:14:55 +0900

    abc012b

--- abc012/b/main.js
+++ abc012/b/main.js
@@ -1,6 +1,12 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
 
+  const h = String(Math.floor(n / 3600)).padStart(2, "0");
+  const m = String(Math.floor((n % 3600) / 60)).padStart(2, "0");
+  const s = String(Math.floor(n % 60)).padStart(2, "0");
+
+  console.log(`${h}:${m}:${s}`);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
