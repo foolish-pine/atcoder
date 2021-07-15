bc8394b79a1beb0fdcb616b99ca8a0c9367654a1
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-15 12:06:40 +0900

    abc142a

--- abc142/a/main.js
+++ abc142/a/main.js
@@ -2,11 +2,9 @@ const main = (args) => {
   const input = args.trim().split("\n");
   const n = Number(input[0]);
 
-  if (n === 1) {
-    console.log(1);
-    return;
-  }
-  console.log(n % 2 === 0 ? n / 2 / n : ((n - 1) / 2 + 1) / n);
+  console.log(
+    n % 2 === 0 ? "0.5000000000" : ((Math.floor(n / 2) + 1) / n).toFixed(10)
+  );
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
