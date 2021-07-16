748dc4b4f829a643ca57d129fbed54d24c7a5689
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-07-16 12:39:21 +0900

    abc141c

--- abc141/c/main.js
+++ abc141/c/main.js
@@ -1,6 +1,26 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0].split(" ")[0]);
+  const k = Number(input[0].split(" ")[1]);
+  const q = Number(input[0].split(" ")[2]);
 
+  let an = Array(n).fill(k);
+  for (let i = 1; i <= q; i++) {
+    an = an.map((a, index) => {
+      if (index + 1 !== Number(input[i])) {
+        return a - 1;
+      } else {
+        return a;
+      }
+    });
+  }
+  an.forEach((a) => {
+    if (a > 0) {
+      console.log("Yes");
+    } else {
+      console.log("No");
+    }
+  });
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
