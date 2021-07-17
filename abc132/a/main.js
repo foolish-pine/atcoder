ec53106f83400598e127a80365deff5df9730258
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-07-17 09:42:40 +0900

    abc132a

--- abc132/a/main.js
+++ abc132/a/main.js
@@ -1,6 +1,12 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
 
+  const map = {};
+  for (let i = 0; i < 4; i++) {
+    map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
+  }
+  console.log(Object.values(map).every((n) => n === 2) ? "Yes" : "No");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
