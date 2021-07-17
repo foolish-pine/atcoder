de087c6f777f8a53bad32cbd01081771843e7d80
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-07-17 09:45:18 +0900

    abc131a

--- abc131/a/main.js
+++ abc131/a/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
 
+  for (let i = 0; i < 3; i++) {
+    if (s[i] === s[i + 1]) {
+      console.log("Bad");
+      return;
+    }
+  }
+  console.log("Good");
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
