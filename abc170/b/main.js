c6f65696b3ec02490c4c6c680b5c97a6d011b5d7
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-07-18 12:13:14 +0900

    abc170b

--- abc170/b/main.js
+++ abc170/b/main.js
@@ -1,6 +1,14 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const [x, y] = input[0].split(" ").map((n) => Number(n));
 
+  console.log(
+    Number.isInteger((y - 2 * x) / 2) &&
+      0 <= (y - 2 * x) / 2 &&
+      (y - 2 * x) / 2 <= x
+      ? "Yes"
+      : "No"
+  );
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
