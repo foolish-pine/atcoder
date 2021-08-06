8a012e2ccf83441d6508506baac0da089089bbf6
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-08-06 12:12:39 +0900

    abc114a

---
+++ abc114/a/main.js
@@ -0,0 +1,12 @@
+const main = (args) => {
+  const input = args.trim().split("\n");
+  const x = Number(input[0]);
+
+  if (x === 7 || x === 5 || x === 3) {
+    console.log("YES");
+  } else {
+    console.log("NO");
+  }
+};
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
