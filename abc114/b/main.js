9920eedd468d5906db147f59cab54cce9b51d44d
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-08-15 14:42:24 +0900

    abc114b

--- abc114/b/main.js
+++ abc114/b/main.js
@@ -1,6 +1,13 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const s = input[0];
 
+  let ans = 1000;
+  for (let i = 0; i <= s.length - 3; i++) {
+    const num = s.slice(i, i + 3);
+    ans = Math.min(ans, Math.abs(753 - Number(num)));
+  }
+  console.log(ans);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
