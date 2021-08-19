8536e679229fb3a43a105c019c2d04396c4cfb24
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-08-19 12:42:16 +0900

    abc102a

--- abc102/a/main.js
+++ abc102/a/main.js
@@ -1,6 +1,8 @@
 const main = (args) => {
   const input = args.trim().split("\n");
+  const n = Number(input[0]);
 
+  console.log(n % 2 === 0 ? n : n * 2);
 };
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
