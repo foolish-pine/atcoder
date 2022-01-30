93228cec6f2543cab4b8219985e7109c7b83a9ba
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-01-30 16:24:28 +0900

    solve abc236c

--- abc236/c/main.py
+++ abc236/c/main.py
@@ -1,2 +1,10 @@
-N = map(int, input.split())
-print(N)
+n, m = map(int, input().split())
+s = list(input().split())
+t = set(input().split())
+
+for n in s:
+    if n in t:
+        print("Yes")
+        t.remove(n)
+    else:
+        print("No")
