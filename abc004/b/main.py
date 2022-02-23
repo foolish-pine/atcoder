50753c98ae5a2c4908a4d6e54301d974dad3d77c
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-23 20:13:57 +0900

    add abc004 and solve abc004b

---
+++ abc004/b/main.py
@@ -0,0 +1,9 @@
+r1 = list(input().split())
+r2 = list(input().split())
+r3 = list(input().split())
+r4 = list(input().split())
+
+print(f"{r4[3]} {r4[2]} {r4[1]} {r4[0]}")
+print(f"{r3[3]} {r3[2]} {r3[1]} {r3[0]}")
+print(f"{r2[3]} {r2[2]} {r2[1]} {r2[0]}")
+print(f"{r1[3]} {r1[2]} {r1[1]} {r1[0]}")
