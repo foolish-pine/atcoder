5c1a22cee9ed6bf58ddd29c2cdf991c8d4110313
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-23 17:42:04 +0900

    solve abc221c

--- abc221/c/main.py
+++ abc221/c/main.py
@@ -1,2 +1,16 @@
-N = map(int, input.split())
-print(N)
+from itertools import permutations
+N = list(input())
+
+ans = 0
+for n in permutations(N):
+    for i in range(1, len(N)):
+        s1 = n[:i]
+        s2 = n[i:]
+        a = ""
+        b = ""
+        for s in s1:
+            a += s
+        for s in s2:
+            b += s
+        ans = max(ans, int(a)*int(b))
+print(ans)
