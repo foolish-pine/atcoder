5f497e6c28c36ddf1d7198920b76eb4454939d01
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-19 18:17:21 +0900

    add abc243 and solve abc243a and abc243b

--- abc239/a/main.py
+++ abc239/a/main.py
@@ -1,4 +1,11 @@
-import math
-h = int(input())
+v, a, b, c = map(int, input().split())
 
-print(math.sqrt(h*(12800000 + h)))
+total = a+b+c
+rem = v % total
+
+if rem-a < 0:
+    print("F")
+elif rem-a-b < 0:
+    print("M")
+elif rem-a-b-c < 0:
+    print("T")
--- abc239/b/main.py
+++ abc239/b/main.py
@@ -1,3 +1,15 @@
-x = int(input())
+n = int(input())
+an = list(map(int, (input().split())))
+bn = list(map(int, (input().split())))
 
-print(x // 10)
+ans1 = 0
+ans2 = 0
+
+for i, a in enumerate(an):
+    if(an[i] == bn[i]):
+        ans1 += 1
+    elif a in bn:
+        ans2 += 1
+
+print(ans1)
+print(ans2)
