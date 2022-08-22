9cf6f1f92710429f4b3981b3dbda2c3da5066269
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-08-22 13:13:55 +0900

    solve abc265b

--- abc265/b/main.py
+++ abc265/b/main.py
@@ -2,7 +2,21 @@
 
 
 def main():
-    pass
+    n, m, t = map(int, input().split())
+    an = [0] + list(map(int, input().split()))
+    bonus = [0] * n
+
+    for _ in range(m):
+        x, y = map(int, input().split())
+        bonus[x - 1] = y
+
+    for i in range(1, n):
+        if t <= an[i]:
+            print("No")
+            exit()
+        t -= an[i]
+        t += bonus[i]
+    print("Yes")
 
 
 main()
