9c5fa4e88477ab420642d6937531871431713e2f
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-08-23 11:36:24 +0900

    solve abc262c

--- abc262/c/main.py
+++ abc262/c/main.py
@@ -2,7 +2,21 @@
 
 
 def main():
-    pass
+    n = int(input())
+    an = list(map(int, input().split()))
+
+    same = 0
+    for i, a in enumerate(an):
+        if a == i + 1:
+            same += 1
+
+    ans = same * (same - 1) // 2
+
+    for i, j in enumerate(an):
+        if i + 1 < j and an[j - 1] == i + 1:
+            ans += 1
+
+    print(ans)
 
 
 main()
