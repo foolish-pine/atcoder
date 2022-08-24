76e1f747a34ccd5c77e520cc36df40483673c705
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-08-24 14:20:35 +0900

    add solve abc252b

--- abc252/b/main.py
+++ abc252/b/main.py
@@ -2,7 +2,19 @@
 
 
 def main():
-    pass
+    n, k = map(int, input().split())
+
+    an = [int(a) for a in input().split()]
+    bn = [int(a) - 1 for a in input().split()]
+
+    max_index = [_ for _, a in enumerate(an) if a == max(an)]
+
+    for i in max_index:
+        if i in bn:
+            print("Yes")
+            exit()
+
+    print("No")
 
 
 main()
