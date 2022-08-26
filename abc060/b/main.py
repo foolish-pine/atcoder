66ad44fa3e1c58e0b553da0c5916a58d04868166
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-08-26 16:01:46 +0900

    solve abc060b

--- abc060/b/main.py
+++ abc060/b/main.py
@@ -2,7 +2,22 @@
 
 
 def main():
-    pass
+    a, b, c = map(int, input().split())
+
+    i = 1
+    l = []
+    while True:
+        sum = a * i
+        if sum % b == c:
+            print("YES")
+            exit()
+        else:
+            if sum % b in l:
+                print("NO")
+                exit()
+            else:
+                l.append(sum % b)
+                i += 1
 
 
 main()
