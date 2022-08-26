de25bf5bcdab5fc00e14ca58152ef16b36f5acae
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-08-26 14:09:01 +0900

    solve abc073c

--- abc073/c/main.py
+++ abc073/c/main.py
@@ -2,7 +2,17 @@
 
 
 def main():
-    pass
+    n = int(input())
+    s = set()
+
+    for _ in range(n):
+        x = input()
+        if x in s:
+            s.discard(x)
+        else:
+            s.add(x)
+
+    print(len(s))
 
 
 main()
