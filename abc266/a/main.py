825ad504192aca556061104564e6467e9e25fb07
foolish-pine <matsuda.takuya41@gmail.com>
2022-09-04 17:07:14 +0900

    add abc266

---
+++ abc266/a/main.py
@@ -0,0 +1,7 @@
+def main():
+    s = input()
+    s_len = len(s)
+    print(s[(s_len + 1) // 2 - 1])
+
+
+main()
---
+++ abc266/b/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def main():
+    n = int(input())
+
+    print(n % 998244353)
+
+
+main()
